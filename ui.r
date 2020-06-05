660efa79fb8d6b48bc917d145954719eff317c14
AashwinS05 <60301730+AashwinS05@users.noreply.github.com>
2020-06-05 17:13:12 -0500

    Add files via upload

--- ui.r
+++ ui.r
@@ -1,135 +1,139 @@
-library(shiny)
-library(networkD3)
-library(sqldf)
-library(visNetwork)
-library(igraph)
-library(sna)
-# datapath <- setwd("C:\\Mydata\\dellstudio\\US_Stuff\\wrk-study\\Study\\Sem 4\\ABI R\\project 1\\shinyapp_4")
+#packages required to be loaded
+packages = c("shiny","shinythemes","rvest","stringr", "tidytext",
+              "sqldf", "ggplot2", "ggthemes", "dplyr","quanteda","tm",
+              "readr", "magrittr", "data.table","xgboost","caret","e1071")
 
-ui <- navbarPage(
-  "AashwinSinghal_Social_Network_Analysis",
-  inverse = TRUE,
-  tabPanel("Load data", fluid = TRUE,
-           titlePanel("Choose file and view data"),
+#Using function to check if each package is on the local machine
+#if a package is installed, it will be loaded
+#if any are not, the missing package(s) will be installed and loaded
+
+package.check = lapply(packages, FUN = function(x) {
+  if (!require(x, character.only = TRUE)) {
+    install.packages(x, dependencies = TRUE)
+    library(x, character.only = TRUE)
+  }
+})
+
+ui <- navbarPage("AashwinSinghal_Natural_Language_Analysis",
+  inverse = TRUE, theme = shinytheme("readable"),
+  
+  tabPanel("Load Data",
+           fluid = TRUE,
+           titlePanel("Download and view the reviews from 
+                      https://www.cars.com/research/toyota-camry/ "),
            sidebarLayout(
-             sidebarPanel(
-               fileInput(inputId = "Emp",
-                         label = " Please Choose file: 'email-Eu-core.txt'",
-                         buttonLabel = "Browse...", 
-                         placeholder = "No file Selected"),
-               fileInput(inputId = "Dept",
-                         label = "Please Choose file: 'email-Eu-core-department-labels.txt'",
-                         buttonLabel = "Browse...",
-                         placeholder = "No file Selected"),
-               sliderInput("Number", 
-                           "Select N to display:",
-                           min = 1, 
-                           max = 30,
-                           value = 10,
-                           sep = '')
-               ),
+             shiny::sidebarPanel(
+               actionButton("action_1",
+                            "Go render Table"),
+               numericInput("numeric_1",
+                            "Choose Rows to Display:",3)),
              mainPanel(
                tabsetPanel(
                  type = "tabs",
-                 tabPanel("email-Eu-core",tableOutput("view_1")),
-                 tabPanel("email-Eu-core-department-labels", tableOutput("view_2"))
-                 )
+                 tabPanel("Reviews Train Data", tableOutput("view_1")),
+                 tabPanel("Reviews Test Data", tableOutput("view_2"))
+               )
                )
              )
            ),
-  tabPanel("Simple Network",
+  tabPanel("Data Preprocessing",
            fluid = TRUE,
-           titlePanel(" Network Model 'n' connections from 'email-Eu-Core.txt' data using VisNetwork"),
-           sidebarLayout(
-             sidebarPanel(
-        # fileInput(inputId = "file3",
-        #                      label = "Choose file:",
-        #                      buttonLabel = "Browse...",
-        #                      placeholder = "No file Selected"),
-               numericInput("obs_1",
-                            "Choose n to display connections:",
-                            10)),
-        mainPanel(visNetworkOutput("simple"))
-        )),
-  tabPanel("Emails Summary",
-           fluid = TRUE,
-           titlePanel("visualization of Email transactions"),
-           sidebarLayout(
-             sidebarPanel(
-               sliderInput("Number_1",
-                           "Select No. of Rows to be used - plot:",
-                           min = 1000,
-                           max = 25571,
-                           value = 2500,
-                           sep = ''),
-               numericInput("obs_2",
-                            "Choose Top N Nodes to display - Table:",
-                            10)),
-             mainPanel(
+           titlePanel(h4("Here I have preprocessed the data in two parts:
+                         Part 1. Reviews have been NORMALIZED to remove 'punctions' & 'Lower-cased'
+                         Part 2. reviews have been TAGGED according to the presence of 
+                            the following words :'service', 'price', 'handling', 'interior' ")),
                tabsetPanel(type = "tabs",
-                           tabPanel("Top Employees - Sent",
-                                    tabsetPanel(type = "tabs",
-                                                tabPanel("table_sent", tableOutput("table_sent")),
-                                                tabPanel("Plot_sent", forceNetworkOutput("Plot_sent")))),
-                           tabPanel("Top Employees - Received",
-                                    tabsetPanel(type ="tabs",
-                                                tabPanel("table_Recvd", tableOutput("table_Recvd")),
-                                                tabPanel("Plot_Recvd", forceNetworkOutput("Plot_Recvd")))))))
+                           tabPanel("Normalized Reviews", fluid = TRUE,
+                                    sidebarLayout(
+                                      shiny::sidebarPanel(
+                                        numericInput("numeric_2",
+                                                     "Choose Rows to Display:",3)),
+                                      mainPanel(
+                                        tabsetPanel(type = "tabs",
+                                                    tabPanel("train", tableOutput("view_3")),
+                                                    tabPanel("Test", tableOutput("view_4")))))),
+                           tabPanel("Tagged Reviews", fluid = TRUE,
+                                    sidebarLayout(
+                                      shiny::sidebarPanel(
+                                        numericInput("numeric_2",
+                                                     "Choose Rows to Display:",3)),
+                                      mainPanel(
+                                        tabsetPanel(type = "tabs",
+                                                    tabPanel("train", tableOutput("view_5")),
+                                                    tabPanel("Test", tableOutput("view_6"))
+                                                    )
+                                        )
+                                      )
+                                    )
+                           )
            ),
-  tabPanel("Centrality",
+  
+  tabPanel("Sentiment Analysis - Train Data",
            fluid = TRUE,
-           titlePanel("Indicators of centrality - Degree & Betweenness"),
-           tabsetPanel(
-             type = "tabs",
-             tabPanel("Degree",
-                      sidebarLayout(
-                        sidebarPanel(
-                          sliderInput("Number_2",
-                                      "Select No. of Rows to be used - plot:",
-                                      min = 1000,
-                                      max = 25571,
-                                      value = 2500,
-                                      sep = '')),
-                        mainPanel(
-                          tabsetPanel(type = "tabs",
-                                      tabPanel("Total - Degree", visNetworkOutput("Total - Degree")),
-                                      tabPanel("In - Degree", visNetworkOutput("In - Degree")))))),
-             tabPanel("Betweenness",
-                      sidebarLayout(
-                        sidebarPanel(
-                          sliderInput("Number_3",
-                                      "Select No. of Rows to be used - plot:",
-                                      min = 1000,
-                                      max = 25571,
-                                      value = 2500,
-                                      sep = '')),
-                        mainPanel(visNetworkOutput("Betweenness"))))
-             )
-           ),
-  tabPanel("Department Level Analysis",
-           fluid = TRUE,
-           titlePanel("Visualization Of Department Level Email transactions"),
+           titlePanel(h4(" ")),
            tabsetPanel(type = "tabs",
-                       tabPanel("Tabular Representation",
-                                sidebarLayout(
-                                  sidebarPanel(
-                                    numericInput("Number_4",
-                                                 "Choose top rows to display - Table:",
-                                                 10)),
-                                  mainPanel(tableOutput("Tabular Representation")))
+                       tabPanel("Sentiment Table With AFINN Scores",
+                                tabsetPanel(type = "tabs",
+                                            tabPanel("Table",
+                                                     sidebarLayout(
+                                                       shiny::sidebarPanel(
+                                                         numericInput("numeric_3",
+                                                                      "Choose Rows to Display:",3)),
+                                                       mainPanel(shiny::tableOutput("view_7")
+                                                                 )
+                                                       )
+                                                     ),
+                                            tabPanel("Vizualization",
+                                                     titlePanel(h4("Although we do see that there are outliers as some 5-star reviews 
+                                                                    have a highly negative sentiment score, 
+                                                                    but still it's a good start! as our sentiment scores are correlated 
+                                                                    with user ratings.")) ,
+                                                     plotOutput("plot_1")
+                                                     )
+                                            )
                                 ),
-                       tabPanel("Plot_Dept",
+                       tabPanel("Analysis - Train Data",
+                                tabsetPanel(type = "tabs",
+                                            tabPanel("Avg Sentiment Rating VS Avg Star Rating",
+                                                     titlePanel(h4("Here it can be see that the average Sentiment rating (Training Data) is pretty close 
+                                                                   to the average Star Rating given by the users - this goes to show that the 
+                                                                   sentiment analysis has been effective !!! ")),
+                                                     tableOutput("view_8")),
+                                            tabPanel("Avg Sentiment Rating/Tag vs Avg Star Ratings/Tag Vs Avg Star Rating(train data)", 
+                                                     titlePanel(h4(" ")),
+                                                     tableOutput("view_9")
+                                                     )
+                                            )
+                                )
+                       )
+           ),
+  tabPanel("Predictive Modelling",
+           fluid = TRUE,
+           titlePanel("Predicted the star Rating using XGBOOST algorithm"),
+           tabPanel("Model Prediction", tableOutput("view_14") , tableOutput("view_15") 
+                    )
+           ),
+  
+  tabPanel("TF - IDF",
+           fluid = TRUE,
+           titlePanel(h4(" ")),
+           tabsetPanel(type = "tabs",
+                       tabPanel("Table",
                                 sidebarLayout(
-                                  sidebarPanel(
-                                    sliderInput("Number_5",
-                                                "Select No. of Rows to be used - plot:",
-                                                min = 1000,
-                                                max = 25571,
-                                                value = 2500,
-                                                sep = '')),
-                                  mainPanel(visNetworkOutput("Plot_Dept"))))
+                                  shiny::sidebarPanel(
+                                    numericInput("numeric_5",
+                                                 "Choose Rows to Display:",10)),
+                                  mainPanel(tabsetPanel(type = "tabs",
+                                                        tabPanel("Service", tableOutput("view_10")),
+                                                        tabPanel("Handling", tableOutput("view_11")),
+                                                        tabPanel("Interior", tableOutput("view_12")),
+                                                        tabPanel("Price", tableOutput("view_13"))
+                                                        )
+                                            )
+                                  )
+                                ),
+                       tabPanel("Vizualization",titlePanel(h4(" ")),
+                                plotOutput("plot_2"))
                        )
            )
-  )
-
-
+)
